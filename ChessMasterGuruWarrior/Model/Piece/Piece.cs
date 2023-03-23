7a6c72992d6ff0614b2b6894692e37ec7e49d52e
anthonyv262 <antvessa6@gmail.com>
2023-03-23 10:28:49 -0500

    3/23: Changes to Model: I added several classes and folders

---
+++ ChessMasterGuruWarrior/Model/Piece/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMasterGuruWarrior.Model.Piece
+{
+    public class Piece
+    {
+        bool IsWhite { get; set; }
+        int PosX { get; set; }
+        int PosY { get; set; }
+        string Type { get; set; }
+
+        public Piece(bool is_white, int posx, int posy, string type)
+        {
+            bool IsWhite = is_white;
+            int PosX = posx;
+            int PosY = posy;
+            string Type = type;
+        }
+
+
+    }
+}
