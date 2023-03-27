a7c53bf78aacbfe96ff8500ba548b978073e9e9c
yotamfre <yotamfre1@gmail.com>
2023-03-27 09:24:54 -0500

    3/27/23 navigation page created

--- ChessMasterGuruWarrior/App.xaml.cs
+++ ChessMasterGuruWarrior/App.xaml.cs
@@ -11,6 +11,8 @@ namespace ChessMasterGuruWarrior
             InitializeComponent();
 
             MainPage = new MainPage();
+
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
