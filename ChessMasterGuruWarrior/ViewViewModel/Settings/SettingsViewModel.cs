8f4d5229fde0d5e5cc7f1c279eb7bd8141c825c9
Brandon Yuan <brandonyuan05@gmail.com>
2023-04-11 10:19:01 -0500

    4/11 not really working settings class

--- ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
+++ ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ChessMasterGuruWarrior.ViewViewModel.Settings
 {
-    class SettingsViewModel
+    public class SettingsViewModel : BaseViewModel
     {
+        public string Username { get; set; }
+        private string _username = string.Empty;
+
+        public SettingsViewModel()
+        {
+            
+        }
+
+        public string UsernameText
+        {
+            get { return _username}
+            set
+            {
+                if (_username != value)
+                    SetProperty(ref _username, value);
+            }
+        }
     }
 }
+
+
