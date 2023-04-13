2138cc2fa5891b73b4817bf1eb2174c3652e08af
yotamfre <yotamfre1@gmail.com>
2023-04-13 09:24:48 -0500

    4/13/23 images folder made

--- ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
+++ ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
@@ -1,30 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Text;
 
 namespace ChessMasterGuruWarrior.ViewViewModel.Settings
 {
-    public class SettingsViewModel : BaseViewModel
+    class SettingsViewModel
     {
-        public string Username { get; set; }
-        private string _username = string.Empty;
-
-        public SettingsViewModel()
-        {
-            
-        }
-
-        public string UsernameText
-        {
-            get { return _username}
-            set
-            {
-                if (_username != value)
-                    SetProperty(ref _username, value);
-            }
-        }
     }
 }
-
-
