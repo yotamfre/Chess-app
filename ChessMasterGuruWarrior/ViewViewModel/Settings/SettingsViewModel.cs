c07b58408ce76626d1f9e9926a972e2185bbb079
Brandon Yuan <brandonyuan05@gmail.com>
2023-04-17 11:12:41 -0500

    4/17 added sqlite

--- ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
+++ ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChessMasterGuruWarrior.ViewViewModel.Base;
+using System;
+using System.ComponentModel;
 
 namespace ChessMasterGuruWarrior.ViewViewModel.Settings
 {
-    class SettingsViewModel
+    public class SettingsViewModel : INotifyPropertyChanged
     {
+        private string _username = "Default Username";
+        private string _email = "placeholder@email.com";
+        public SettingsViewModel()
+        {
+
+        }
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (_username != value)
+                    SetProperty(ref _username, value);
+                SetProperty(ref _username, value); //SQL
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
