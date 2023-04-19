8f83d020c60b17bd27d5b2b49760b75c8e67bef2
Brandon Yuan <brandonyuan05@gmail.com>
2023-04-19 10:28:54 -0500

    4/19 no more sqlite in settings -> do update button that send info instead

--- ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
+++ ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
@@ -4,25 +4,20 @@ using System.ComponentModel;
 
 namespace ChessMasterGuruWarrior.ViewViewModel.Settings
 {
-    public class SettingsViewModel : INotifyPropertyChanged
+    public class SettingsViewModel 
     {
         private string _username = "Default Username";
         private string _email = "placeholder@email.com";
+
         public SettingsViewModel()
         {
 
         }
-        public string Username
-        {
-            get { return _username; }
-            set
-            {
-                if (_username != value)
-                    SetProperty(ref _username, value);
-                SetProperty(ref _username, value); //SQL
-            }
-        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        
     }
+
+
+
+    
 }
