24291520b9c0e24d4040917aeb7328ac9885a822
Brandon Yuan <brandonyuan05@gmail.com>
2023-04-24 10:22:18 -0500

    4/24 main page fixed

---
+++ ChessMasterGuruWarrior/ViewViewModel/MainPage/MainPageView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChessMasterGuruWarrior.ViewViewModel.MainPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageView : ContentPage
+    {
+        public MainPageView()
+        {
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+
+        public MainPageView(string username)
+        {
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+    }
+}
--- ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
+++ ChessMasterGuruWarrior/ViewViewModel/Settings/SettingsViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using ChessMasterGuruWarrior.ViewViewModel.Base;
+using ChessMasterGuruWarrior.ViewViewModel.MainPage;
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ChessMasterGuruWarrior.ViewViewModel.Settings
 {
-    public class SettingsViewModel : INotifyPropertyChanged
+    public class SettingsViewModel : BaseViewModel
     {
+        public ICommand OnEntryClicked { get; }
+
         private string _username = "Default Username";
         private string _email = "placeholder@email.com";
 
         public SettingsViewModel()
         {
-
+            OnEntryClicked = new Command(OnEntryClickedAsync);
         }
 
         public string Username
@@ -22,19 +27,23 @@ namespace ChessMasterGuruWarrior.ViewViewModel.Settings
             {
                 if (_username != value)
                     SetProperty(ref _username, value);
-                SetProperty(ref _username, value); //SQL
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+
+        public void OnEntryClickedAsync(object obj)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            Application.Current.MainPage.Navigation.PushAsync(new MainPageView(_username));
         }
+
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected void OnPropertyChanged([CallerMemberName] string name = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        //}
+
     }
-
-
-
-    
 }
